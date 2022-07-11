6369a1b3b9e76dc96801b2c2b7d52074b02ebf43
Mateusz Hasik <matihasek@gmail.com>
2022-07-11 20:13:59 +0200

    middleware configuration

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using RestaurantAPI;
 using AutoMapper;
 using System.Reflection;
 using RestaurantAPI.Services;
+using RestaurantAPI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
 builder.Services.AddScoped<IDishService, DishService>();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
 var app = builder.Build();
 
@@ -22,6 +24,7 @@ if (!app.Environment.IsDevelopment())
 
     app.UseHsts();
 }
+app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
